5903f074eb55887f539243d5f1e6321c99d24735
Jose A. Álvarez <infoiguacel@gmail.com>
2025-02-12 10:59:24 +0100

    cover

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,4 @@ export const siteTitle = 'Graphics feed';
 export const siteDescription = "Graphics";
 export const siteURL = 'https://iguacel.github.io/graphics-feed/';
 
-export const siteImage = 'https://iguacel.github.io/graphics-feed/cover.jpg';
+export const siteImage = 'https://iguacel.github.io/graphics-feed/cover.png';

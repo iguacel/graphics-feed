565c5a34e678e2294055b596b67079d7df9c177e
Jose A. Álvarez <infoiguacel@gmail.com>
2025-02-24 22:35:20 +0100

    Updates

--- _app/immutable/nodes/4.BvVO1067.js
+++ _app/immutable/nodes/4.BvVO1067.js
@@ -1,4 +1,4 @@
-import{t as p,a as i}from"../chunks/GFEvi2bW.js";import"../chunks/Bmv_rN7v.js";import{l as s,r as c,m as v}from"../chunks/BRJUpaFT.js";import{s as l}from"../chunks/D5l_xSw9.js";import{b as t}from"../chunks/BMYS0dRJ.js";var b=p(`<code class="svelte-1li1x0m"><pre>
+import{t as p,a as i}from"../chunks/CFuzDKZG.js";import"../chunks/YO9hDPOy.js";import{l as o,m as c,s as v}from"../chunks/DMbZFwqQ.js";import{s as l}from"../chunks/DsZfnUb5.js";import{b as t}from"../chunks/DB-ghqqH.js";var b=p(`<code class="svelte-1li1x0m"><pre>
     0000000                         000        0000000
   111111111      11111111100          000      111111111
   00000        111111111111111111      00000      000000
@@ -41,4 +41,4 @@ import{t as p,a as i}from"../chunks/GFEvi2bW.js";import"../chunks/Bmv_rN7v.js";i
                              ████▀▐          
                              ██▀             
              ▀
-    </pre></code>`);function $(e){var a=_();i(e,a)}var g=p('<main class="svelte-1q0ockc"><div class="grid svelte-1q0ockc"><a class="placeholder"><!></a> <a class="placeholder"><!></a> <a class="placeholder"><h1 class="svelte-1q0ockc">Reuters</h1></a> <a class="placeholder"><h1 class="svelte-1q0ockc">Bloomberg</h1></a></div></main>');function y(e){var a=g(),d=s(a),r=s(d);l(r,"href",`${t??""}/wp`);var n=s(r);$(n),c(r);var o=v(r,2);l(o,"href",`${t??""}/nyt`);var h=s(o);u(h),c(o);var m=v(o,2);l(m,"href",`${t??""}/reuters`);var f=v(m,2);l(f,"href",`${t??""}/bloomberg`),c(d),c(a),i(e,a)}export{y as component};
+    </pre></code>`);function $(e){var a=_();i(e,a)}var g=p('<main class="svelte-1q0ockc"><div class="grid svelte-1q0ockc"><a class="placeholder"><!></a> <a class="placeholder"><!></a> <a class="placeholder"><h1 class="svelte-1q0ockc">Reuters</h1></a> <a class="placeholder"><h1 class="svelte-1q0ockc">Bloomberg</h1></a></div></main>');function y(e){var a=g(),d=o(a),r=o(d);l(r,"href",`${t??""}/wp`);var n=o(r);$(n),c(r);var s=v(r,2);l(s,"href",`${t??""}/nyt`);var h=o(s);u(h),c(s);var m=v(s,2);l(m,"href",`${t??""}/reuters`);var f=v(m,2);l(f,"href",`${t??""}/bloomberg`),c(d),c(a),i(e,a)}export{y as component};

423bb3aece5195bb295fda0ee3227b37a791cfa2
Jose A. Álvarez <infoiguacel@gmail.com>
2025-06-23 17:11:13 +0200

    Updates

---
+++ _app/immutable/entry/start.Kovv1yJq.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/BAP90MRr.js";export{t as start};
---
+++ _app/immutable/nodes/5.CIIwVglj.js
@@ -0,0 +1,44 @@
+import{t as p,a as d}from"../chunks/CrCFE0wT.js";import"../chunks/DTu2euSP.js";import{l as c,m as v,s as l}from"../chunks/DRmtHWZe.js";import{s as a}from"../chunks/Dlof71Xr.js";import{b as r}from"../chunks/il9Ady3j.js";var $=p(`<code class="svelte-1li1x0m"><pre>
+    0000000                         000        0000000
+  111111111      11111111100          000      111111111
+  00000        111111111111111111      00000      000000
+  000        1111111111111111111111111100000         000
+  000        1111       1111111111111111100          000
+  000         11       0     1111111100              000
+  000          1      00             1               000
+  000               00      00       1               000
+  000             000    00000       1               000
+00000            0000  00000000       1                00000
+11111            000 00    000000      000                 11111
+00000          0000      000000     00000              00000
+  000        10000      000000      000              0000
+  000        00000      000000       1               000
+  000        000000     10000        1     0         000
+  000        1000000 00              1    00         000
+  000         1111111                1 0000          000
+  000          1111111100           000000           000
+  0000          111111111111111110000000            0000
+  111111111        111111111111100000          111111111
+    0000000              00000000              0000000
+
+</pre></code>`);function b(s){var e=$();d(s,e)}var g=p(`<code class="svelte-jp61u2"><pre>    ▄█▀                                     
+    ▄██                                      
+   ████▄                                     
+    █████   ▄█▄     ▄█▄      ▄█▄  ▐   ▄█▄    
+     ████ ▄█████▄ ▄█████▄  ▄█████▌▐ ▄█████▄  
+     ████▀  ▀████▀ ▀█████    ████▌▐▀  ▀████  
+     ████    ████   ▐████    ████▌▐    ████  
+    ▄████    ████   ▐████  ▄█████▌▐    ████  
+  ▄██████    ████   ▐████ ███████▌▐    ████  
+  █▀ ████    ████   ▐████ ▀  ████▌▐    ████  
+     ████    ████   ▐████    ████▌▐    ████  
+     ████    ████   ▐████    ████▌▐    ████  
+     █████▄  █████▄ ▐██▀    ▄████▌▐▄▄  ████  
+     ▀███████▀███████▀    ▄██████▌▐███▄██▀   
+       ▀███▀   ▀███▀      █▀▀████▌▐▀███▀     
+         ▀       ▀           ████▌▐  ▀       
+                             ████▌▐          
+                             ████▀▐          
+                             ██▀             
+             ▀
+    </pre></code>`);function P(s){var e=g();d(s,e)}var x=p('<main class="svelte-v5lfer"><div class="grid svelte-v5lfer"><a class="placeholder"><!></a> <a class="placeholder"><!></a> <a class="placeholder"><h1 class="svelte-v5lfer">Reuters</h1></a> <a class="placeholder"><h1 class="svelte-v5lfer">Bloomberg</h1></a> <a class="placeholder"><h1 class="svelte-v5lfer">SCMP</h1></a> <a class="placeholder"><h1 class="svelte-v5lfer">The Pudding</h1></a></div></main>');function C(s){var e=x(),h=c(e),o=c(h);a(o,"href",`${r??""}/wp`);var n=c(o);P(n),v(o);var t=l(o,2);a(t,"href",`${r??""}/nyt`);var u=c(t);b(u),v(t);var i=l(t,2);a(i,"href",`${r??""}/reuters`);var f=l(i,2);a(f,"href",`${r??""}/bloomberg`);var m=l(f,2);a(m,"href",`${r??""}/scmp`);var _=l(m,2);a(_,"href",`${r??""}/thepudding`),v(h),v(e),d(s,e)}export{C as component};
